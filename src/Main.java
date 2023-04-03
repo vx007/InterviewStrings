e07431743fa709ce1f386814669e0228d0807c32
vx007 <vasilyev.x@mail.ru>
2023-04-03 19:48:14 +0300

    Commit #2

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,4 @@
-import java.util.Arrays;
-import java.util.stream.Collectors;
-
 public class Main {
     public static void main(String[] args) {
-//        Дан массив строк, необходимо удалить из него все дубли,
-//        оставшиеся строки объединить в одну в порядке следования в массиве.
-        String[] strings = {"a", "b", "a", "c", "b", "d", "e", "b", "a", "e"};
-        String result = Arrays.stream(strings).distinct().collect(Collectors.joining());
-        System.out.println(result);
     }
 }
